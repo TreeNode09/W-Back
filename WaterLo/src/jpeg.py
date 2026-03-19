b64dde3b2f88704d3ac8bd9d46dd037df6d438ed
TreeNode09 <StickpageX@hotmail.com>
2026-03-18 23:11:01 -0700

    augly报错，改用PIL实现JPEG压缩

--- WaterLo/src/jpeg.py
+++ WaterLo/src/jpeg.py
@@ -1,6 +1,8 @@
 import torch
 import torchvision.transforms as T
-import augly.image as imaugs
+# import augly.image as imaugs
+from PIL import Image
+import io
 
 
 def add_jpeg_noise(im: torch.tensor, device, quality: int = 100):
@@ -8,7 +10,14 @@ def add_jpeg_noise(im: torch.tensor, device, quality: int = 100):
     jpegs = []
     for i in range(im_r.shape[0]):
         pil = T.ToPILImage()(im_r[i])
-        jpeg = imaugs.functional.encoding_quality(pil, quality=quality)
+        # jpeg = imaugs.functional.encoding_quality(pil, quality=quality)
+
+        # Compress with PIL
+        buffer = io.BytesIO()
+        pil.save(buffer, format='JPEG', quality=quality)
+        buffer.seek(0)
+        jpeg = Image.open(buffer)
+        
         jpegs.append(T.ToTensor()(jpeg))
     jpegs = torch.stack(jpegs).to(device)
     return im + (jpegs - im.detach())
